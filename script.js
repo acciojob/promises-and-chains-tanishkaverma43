d9188c910b54382906c11e21b44a04bfd43c4475
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-11 19:10:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function(event){
+  event.preventDefault()
+  let age = document.getElementById("age").value;
+  let name = document.getElementById("name").value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000)
+  });
+
+  promise.then(data => {
+    alert(data);
+  }).catch(error => {
+    alert(error);
+  });
+});
